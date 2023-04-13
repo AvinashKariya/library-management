51429c7c9af65cdfd67c888d5712063a1945311e
AvinashKariya <83137333+AvinashKariya@users.noreply.github.com>
2023-04-13 19:29:02 +0530

    Update DbConnect.java

--- src/JFrames/DbConnect.java
+++ src/JFrames/DbConnect.java
@@ -4,7 +4,7 @@ public class DbConnect {
     static Connection con = null;
     public static Connection getConnection(){
         try{
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/lib_mng","root","Avinash@sql78");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/lib_mng","root","root");
         }catch(SQLException e){
             System.out.println(e);
         }
